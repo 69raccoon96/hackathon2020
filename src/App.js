bb9a6b0ea58cb2977702b4d48bb755da221fa360
Ivan Grabovsky <raccoon.twitch@gmail.com>
2020-10-24 19:35:58 +0500

    fix upper row

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ function App() {
     <div className="App">
       <header className="App-header">
           <div className="box">
-              <MyComponent></MyComponent>
+              <UpperRow></UpperRow>
           </div>
       </header>
     </div>
   );
 }
-const MyComponent = ({ logged }) => {
-    if (logged) {
+const UpperRow = ({ logged }) => {
+    if (true) {
         return (
-            <div className="hello">
+            <div className="upper-row">
+                <div className="not-auth-box">
+                    <div className="not-auth-text">ВЫ АВТОРИЗОВАНЫ</div>
 
+                </div>
+                <button className="not-auth-button">
+                    <span className="not-auth-button-text">Имя</span>
+                </button>
             </div>
         );
     }
