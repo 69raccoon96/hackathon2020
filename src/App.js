ad060e887b11fbfe82ffbe31617a7e7bb024d842
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-10-25 02:24:43 +0500

    BootStrapSomeShit added

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
 import './App.css';
 import './Register.js'
 import Register from "./Register";
+import Main from "./Main";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Register></Register>
-          {/*<div className="box">*/}
-          {/*    <UpperRow></UpperRow>*/}
-          {/*    <div className="middle-part-startpage">*/}
-          {/*        <div className="middle-part-tet">*/}
-          {/*            ИЩЕШЬ<br/>МАГАЗИН?</div>*/}
-          {/*        <button className="middle-part-button">*/}
-          {/*            <div className="middle-part-text">ищу!</div></button>*/}
-          {/*    </div>*/}
-          {/*    <div className="bottom-block">*/}
-          {/*        <button className="bottom-button">*/}
-          {/*            <div className="bottom-button-text">Любимые места</div>*/}
-          {/*        </button>*/}
-          {/*        <button className="bottom-button">*/}
-          {/*            <div className="bottom-button-text">История посещений</div>*/}
-          {/*        </button>*/}
-          {/*    </div>*/}
-          {/*</div>*/}
+          <Main/>
+
       </header>
     </div>
   );
 }
-const UpperRow = ({ logged }) => {
-    if (true) {
-        return (
-            <div className="upper-row">
-                <div className="not-auth-box">
-                    <div className="not-auth-text">ВЫ АВТОРИЗОВАНЫ</div>
 
-                </div>
-                <button className="not-auth-button">
-                    <span className="not-auth-button-text">ИМЯ</span>
-                </button>
-            </div>
-        );
-    }
-    return (
-        <div className="upper-row">
-        <div className="not-auth-box">
-            <div className="not-auth-text">
-                ВЫ НЕ АВТОРИЗОВАНЫ
-                </div>
-
-        </div>
-            <button className="not-auth-button">
-                <span className="not-auth-button-text">ВОЙТИ</span>
-            </button>
-        </div>
-    );
-};
 export default App;
--- src/Register.js
+++ src/Register.js
@@ -1,31 +1,33 @@
 import "./Register.css"
-import { Icon, Input } from "semantic-ui-react";
+import {Icon, Input} from "semantic-ui-react";
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Register() {
     return (
-        <div className="box">
-            <div className="box-reg">
+        <div className="row">
+            <div className="box-reg row-1">
                 <div className="text-reg"> Авторизация</div>
                 <hr className="box-line"/>
                 <div className="box-data">
-                <div className="box-before-mail">
-                <div className="box-mail">
-                    <Icon name="mail" color="grey" />
-                    <input className="input-box" value="Почта"/>
-                </div>
-                </div>
-                <div className="box-before-mail">
-                    <div className="box-mail">
-                        <Icon name="key" color="grey" />
-                        <input className="input-box" type="password" value="Пароль"/>
+                    <div className="box-before-mail">
+                        <div className="box-mail">
+                            <Icon name="mail" color="grey"/>
+                            <input className="input-box" value="Почта"/>
+                        </div>
+                    </div>
+                    <div className="box-before-mail row-1">
+                        <div className="box-mail">
+                            <Icon name="key" color="grey"/>
+                            <input className="input-box" type="password" value="Пароль"/>
+                        </div>
                     </div>
                 </div>
-                </div>
                 <div className="button-login">
-                <button>Войти</button>
+                    <button>Войти</button>
                 </div>
             </div>
         </div>
-    )};
+    )
+}
 export default Register;
