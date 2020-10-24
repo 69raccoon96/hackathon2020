2ed04113070c0bf8f65122216808000079eaf816
Ivan Grabovsky <raccoon.twitch@gmail.com>
2020-10-24 19:07:49 +0500

    start work with first page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,30 @@ function App() {
     <div className="App">
       <header className="App-header">
           <div className="box">
-              "Hello"
+              <MyComponent></MyComponent>
           </div>
       </header>
     </div>
   );
 }
+const MyComponent = ({ logged }) => {
+    if (logged) {
+        return (
+            <div className="hello">
 
+            </div>
+        );
+    }
+    return (
+        <div className="upper-row">
+        <div className="not-auth-box">
+            <div className="not-auth-text">ВЫ НЕ АВТОРИЗОВАНЫ</div>
+
+        </div>
+            <button className="not-auth-button">
+                <span className="not-auth-button-text">ВОЙТИ</span>
+            </button>
+        </div>
+    );
+};
 export default App;
