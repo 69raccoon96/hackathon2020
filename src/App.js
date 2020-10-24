ef6b594ae1edbaac3d6938cf9d08fd871aa9a17a
Ivan Grabovsky <raccoon.twitch@gmail.com>
2020-10-25 04:03:28 +0500

    added shops

--- src/App.js
+++ src/App.js
@@ -2,31 +2,22 @@ import './App.css';
 import './Register.js'
 import Register from "./Register";
 import Main from "./Main";
+import Categories from "./Categories.js"
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <Register></Register>
-          {/*<div className="box">*/}
-          {/*    <UpperRow></UpperRow>*/}
-          {/*    <div className="middle-part-startpage">*/}
-          {/*        <div className="middle-part-tet">*/}
-          {/*            ИЩЕШЬ<br/>МАГАЗИН?</div>*/}
-          {/*        <button className="middle-part-button">*/}
-          {/*            <div className="middle-part-text">ищу!</div></button>*/}
-          {/*    </div>*/}
-          {/*    <div className="bottom-block">*/}
-          {/*        <button className="bottom-button">*/}
-          {/*            <div className="bottom-button-text">Любимые места</div>*/}
-          {/*        </button>*/}
-          {/*        <button className="bottom-button">*/}
-          {/*            <div className="bottom-button-text">История посещений</div>*/}
-          {/*        </button>*/}
-          {/*    </div>*/}
-          {/*</div>*/}
+    <Categories></Categories>
       </header>
     </div>
   );
+}
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <Register />;
+    }
+    return <Main />;
 }
 
 export default App;
